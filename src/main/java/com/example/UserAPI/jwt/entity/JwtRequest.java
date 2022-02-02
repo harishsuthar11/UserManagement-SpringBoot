2f0ff05e0af1067d024b3050b765e7d48ba8e2fb
harishsuthar11 <harishsuthar000@gmmail.com>
2022-02-02 09:31:55 +0530

    added jwt

--- src/main/java/com/example/UserAPI/jwt/entity/JwtRequest.java
+++ src/main/java/com/example/UserAPI/jwt/entity/JwtRequest.java
@@ -1,4 +1,9 @@
 package com.example.UserAPI.jwt.entity;
 
+import lombok.Data;
+
+@Data
 public class JwtRequest {
+    private String username;
+    private String password;
 }
---
+++ src/main/java/com/example/UserAPI/jwt/entity/JwtResponse.java
@@ -0,0 +1,9 @@
+package com.example.UserAPI.jwt.entity;
+
+import lombok.Data;
+
+@Data
+public class JwtResponse {
+    private final String jwt;
+
+}
